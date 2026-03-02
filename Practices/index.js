581f7b0149b3d84d23559c1b655ce03786ea43ad
MahinAhmed-bd <mahintaskinff@gmail.com>
2026-03-02 10:35:49 +0600

    This is a practice page of javascript for web dev.

---
+++ Practices/index.js
@@ -0,0 +1,72 @@
+
+// function addNumber(a,b){
+//     let sum=a+b;
+//     return sum;
+// }
+// const result=addNumber(3,4);
+// console.log(result);
+
+
+// const addNumber=(a,b)=>{
+//     let sum=a+b;
+//     return sum;
+// }
+// const result=addNumber(5,10);
+// console.log(result);
+// const button = document.querySelector("#red");
+
+
+// function changeBG(){
+//     document.body.style.backgroundColor=color;
+// }
+// button.addEventListener("click", changeBG);
+// button.addEventListener("click", changeBg2);
+// button.addEventListener("click", changeBG3);
+
+
+// let sum = 0;
+// let i;
+// let n = Number(prompt("Enter the value of n :"));
+// for (i=0 ; i<=n ; i++){
+//     sum+=i;
+//     console.log((n),"+");
+// }
+// console.log(sum);
+
+// let i = 0;
+// do {
+//   i += 1;
+//   console.log(i);
+// } while (i < 6);
+
+
+// Question number one
+
+// for (i=1; i<11;i++){
+//     console.log(i)
+// }
+
+// Question number two
+
+// let i=10;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+
+// Question number three
+// for(i=1; i<=20; i++ ){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+    
+// }
+
+// Question number four
+let i=1;
+while(i<=15){
+    if(i%2!==0){
+    console.log("Odd numbers are:"+i);
+    }
+    i++;
+}
