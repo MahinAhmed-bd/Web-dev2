16e88833b77b4b0e9d109c6eca50e96af371cf13
MahinAhmed-bd <mahintaskinff@gmail.com>
2026-04-06 00:03:02 +0600

    practicing eventListener in javascript

--- Practices/index.js
+++ Practices/index.js
@@ -143,7 +143,14 @@
 // });
 
 
-
+const inp = document.querySelector("input");
+inp.addEventListener("input", (dets) =>{
+    if (dets.data !== null){
+        
+        console.log(dets.data);
+    }
+    
+});
 
 
 
